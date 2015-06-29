7c2134e322f6e47e3f29c0bafd4c9700fb7b127e
Ricardo Rossi <rrossi@endata.com>
2015-06-28 20:00:20 -0500

    Cleaned up Routable facade

--- router.go
+++ router.go
@@ -2,17 +2,8 @@ package gopher
 
 import (
 	"github.com/gopherlabs/gopher/contracts"
-	"net/http"
 )
 
-func NewRouter() contracts.Routerable {
+func NewRouter() contracts.Routable {
 	return GetContext().Router.NewRouter()
-}
-
-func Get(path string, fn func(http.ResponseWriter, *http.Request)) contracts.Routerable {
-	return GetContext().Router.Get(path, fn)
-}
-
-func Serve() {
-	GetContext().Router.Serve()
 }
