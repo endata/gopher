0d1e0b6bad8da121401013a2f13daeb3f42d6ff6
Ricardo Rossi <rrossi@endata.com>
2015-06-29 00:17:58 -0500

    Added logger constructor

--- contracts/loggable.go
+++ contracts/loggable.go
@@ -1,22 +1,17 @@
 package contracts
 
 type Loggable interface {
-	Hello(msg string)
+	NewLogger() Loggable
 
-	//
-	//	Info(msg string, args ...interface{})
-	//	//
-	//	Debug(msg string, args ...interface{})
-	//
-	//	//
-	//	Warn(msg string, args ...interface{})
-	//
-	//	//
-	//	Error(msg string, args ...interface{})
-	//
-	//	//
-	//	Trace(msg string, args ...interface{})
-	//
-	//	//
-	//	Fatal(msg string, args ...interface{})
+	Info(msg string, args ...interface{})
+
+	Debug(msg string, args ...interface{})
+
+	Warn(msg string, args ...interface{})
+
+	Error(msg string, args ...interface{})
+
+	Fatal(msg string, args ...interface{})
+
+	Panic(msg string, args ...interface{})
 }
