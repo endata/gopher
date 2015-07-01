36fd8af39287cd8a2721a17185e65a38e1dfd5bd
Ricardo Rossi <ricardo.rossi@availity.com>
2015-06-30 19:16:05 -0500

    fixed test

--- container_test.go
+++ container_test.go
@@ -2,12 +2,10 @@ package gopher
 
 import (
 	"testing"
-
-	"github.com/gopherlabs/gopher"
 )
 
 func TestNewApp(t *testing.T) {
-	app := gopher.App()
+	app := App()
 	if app == nil {
 		t.Error("Expected to get a pointer to the container, but got nil instead")
 	}
