40427ac0e6acbf1bffa07edbc01bee2ee02d3879
Ricardo Rossi <ricardo.rossi@availity.com>
2015-07-01 21:26:29 -0500

    Minor change

--- config.go
+++ config.go
@@ -4,10 +4,7 @@ var defaultConfig = map[string]map[string]interface{}{
 	LOGGER: {
 		"FullTimestamp": true,
 	},
-	ROUTER: {
-		"name": "Router",
-	},
 	RENDERER: {
-		"provider": "Renderer",
+		"ViewsDir": "templates",
 	},
 }
